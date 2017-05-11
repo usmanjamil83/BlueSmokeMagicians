68aac75517f8701cc8c70f5b68f7c8704541a58c
usmanjamil83 <usmanjamil83@hotmail.com>
2017-05-11 15:31:56 -0400

    created folders

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 
 // Sets up the Express App
 // =============================================================
@@ -15,14 +16,18 @@ var PORT = process.env.PORT || 8080;
 // Requiring our models for syncing
 var db = require("./models");
 
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static(process.cwd() + "/public"));
 
-// Static directory
-app.use(express.static("./public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Routes =============================================================
 
@@ -34,8 +39,7 @@ require("./routes/author-api-routes.js")(app);
 db.sequelize.sync(
 	// { force: true } ??
 	).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-});
-
+		app.listen(PORT, function() {
+			console.log("App listening on PORT " + PORT);
+		});
+	});
