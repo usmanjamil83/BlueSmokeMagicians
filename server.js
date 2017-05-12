aaca6307556a8e0c7ee9ebd5353cf5311d44ba98
christinegillis <chrisa8300@gmail.com>
2017-05-11 23:09:04 -0400

    fixes server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Blue Smoke Magicians server setup
 var express = require("express");
 var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 
 // Sets up the Express App
 // =============================================================
@@ -10,23 +11,22 @@ var PORT = process.env.PORT || 8080;
 // Requiring our models for syncing
 var db = require("./models");
 
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static(process.cwd() + "/public"));
 
-// Static directory
-app.use(express.static("./public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// Routes =============================================================
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
 
+// Routes 
 require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+require("./routes/post-api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our express app
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-});
+db.sequelize.sync(
+	// { force: true } ??
+	).then(function() {
+		app.listen(PORT, function() {
+			console.log("App listening on PORT " + PORT);
+		});
+	});
