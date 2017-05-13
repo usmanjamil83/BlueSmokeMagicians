44817943f0151fe19bcc766e36ed7c0ff12fb6fe
ValHall <Valynhall@gmail.com>
2017-05-13 11:08:46 -0400

    added gender option to form

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -1,4 +1,8 @@
-// Initialize collapse button
-  $(".button-collapse").sideNav();
-  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-  $('.collapsible').collapsible();
+$(document).ready(function() {
+    $('select').material_select();
+
+    // Initialize collapse button
+    $(".button-collapse").sideNav();
+    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+    $('.collapsible').collapsible();
+});
