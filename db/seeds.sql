a94d6f937584b008912831324101aa2f9383c8a1
usmanjamil83 <usmanjamil83@hotmail.com>
2017-05-14 19:13:31 -0400

    seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,10 @@
--- Use github_db.User;
--- INSERT into github_db.User (name, age, gender, tagline, image, match) VALUES ("jdoe", 25, "Female", "hello", "home/christine/Pictures/Screenshot.png", "person1");
+INSERT INTO users(name, age, gender, quote, image)
+VALUES("jdoe", 25, "Female", "hello", "home/christine/Pictures/Screenshot.png");
+INSERT INTO users(name, age, gender, quote, image)
+VALUES("ali", 35, "male", "hi", "home/christine/Pictures/Screenshot.png");
+INSERT INTO users(name, age, gender, quote, image)
+VALUES("joe", 50, "male", "what", "home/christine/Pictures/Screenshot.png");
+INSERT INTO users(name, age, gender, quote, image)
+VALUES("joy", 40, "Female", "who", "home/christine/Pictures/Screenshot.png");
+INSERT INTO users(name, age, gender, quote, image)
+VALUES("mike", 30, "male", "this", "home/christine/Pictures/Screenshot.png");
