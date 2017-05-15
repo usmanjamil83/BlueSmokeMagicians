15df33854dc403da08d61254ca6f5b72204bbd85
M R <M R>
2017-05-14 23:02:58 -0400

    interim changes to question page

---
+++ db/sampleusers.js
@@ -0,0 +1,83 @@
+var friendsResponseArray = [{
+    "name": "Halle",
+    "photo": "http://i.dailymail.co.uk/i/pix/2015/08/18/09/2B74009E00000578-3201718-image-a-2_1439885890058.jpg",
+    "responseObj": [
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5"
+    ]
+  },
+  {
+    "name": "Meagan Good",
+    "photo": "http://www.indiewire.com/wp-content/uploads/2015/02/meagan-good.jpg",
+    "responseObj": [
+      "2",
+      "2",
+      "2",
+      "2",
+      "3",
+      "2",
+      "2",
+      "2",
+      "2",
+      "2"
+    ]
+  },
+  {
+    "name": "Penelope",
+    "photo": "http://eluxemagazine.com/wp-content/uploads/2015/06/Pene%CC%81lope_Cruz_-_Cannes_2011.jpg",
+    "responseObj": [
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4"
+    ]
+  },
+  {
+    "name": "Beyonce",
+    "photo": "http://hellobaby.hu/wp-content/uploads/2017/02/1-4.jpg",
+    "responseObj": [
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3"
+    ]
+  },
+  {
+    "name": "Monica Bellucci",
+    "photo": "http://unitedbloggers.no/uploads/sites/623/2016/12/monica.png",
+    "responseObj": [
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1"
+    ]
+  }
+];
+
+module.exports = friendsResponseArray;
---
+++ public/assets/js/questions.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  // Create global variables including those to hold responses to questions
+  var currentMatchPoints = 0;
+  var q01Answer = 0;
+  var q02Answer = 0;
+  var q03Answer = 0;
+  var q04Answer = 0;
+  var q05Answer = 0;
+  var q06Answer = 0;
+  var responses = [];
+
+});
