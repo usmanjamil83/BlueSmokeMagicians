219a99ae52ff9624af2e7432f6cf0ca6c0823ea3
ValHall <Valynhall@gmail.com>
2017-05-16 09:32:01 -0400

    grabbing values - not able to get ID of user - importing issue

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -1,33 +1,24 @@
 $(document).ready(function() {
-    $('select').material_select();
+  $('select').material_select();
 
-    // Initialize collapse button
-    $(".button-collapse").sideNav();
-    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-    $('.collapsible').collapsible();
+  // Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  $('.collapsible').collapsible();
 
-    var config = {
-      '.chosen-select'           : {},
-      '.chosen-select-deselect'  : {allow_single_deselect:true},
-      '.chosen-select-no-single' : {disable_search_threshold:10},
-      '.chosen-select-no-results': {no_results_text:'Oops, nothing found!'},
-      '.chosen-select-width'     : {width:"95%"}
-    }
-    for (var selector in config) {
-      $(selector).chosen(config[selector]);
-    }
+
 
     // Capture the form inputs
-    $("#submit-button").on("click", function(e){
+    $("#submit_btn").on("click", function(e){
       e.preventDefault();
-      console.log('test');
       // Form validation
       function validateForm() {
       var isValid = true;
-      $('.validate').each(function() {
-        if ( $(this).val() === '' )
-            isValid = false;
-      });
+      // $('.validate').each(function() {
+      //   if ( $(this).val() === '' )
+      //       console.log('Did not populate: ' + $(this));
+      //       isValid = false;
+      // });
 
       $('.chosen-select').each(function() {
 
@@ -45,33 +36,33 @@ $(document).ready(function() {
           // img: $("img"),
           name: $("#name").val(),
           quote: $("#quote").val(),
-          gender: [$("#Agender").val(), $("#Androgynous").val(), $("#Female").val(), $("#Male").val(), $("#Pangender").val(),
-          $("#Transgender").val(), $("#Transsexual").val()]
-        }
+          gender: $("#gender").val(),
+          image: $("#image").val(),
+          age: $("#age").val()
+        };
 
 
         // Grab the URL of the website
         var currentURL = window.location.origin;
-
+        console.log("Endpoint: " + currentURL + "/api/blah");
         // AJAX post the data to the friends API.
-    //     $.post(currentURL + "/api/", userData, function(data){
-    //       console.log("data: " + data.name);
-    //       // Grab the result from the AJAX post so that the best match's name and photo are displayed.
-    //       $("#match-name").text(data.name);
-    //       $('#match-image').attr("src", data.image);
-    //
-    //       // Show the modal with the best match
-    //       $("#resultsModal").modal('toggle');
-    //
-    //     });
-    // }
-    // else
-    // {
-    //   alert("Please fill out all fields before submitting!");
-    // }
-    //
-    //   return false;
-    // });
+        $.post(currentURL + "/api/users", userData, function(data){
+          console.log("dbnvfata: " + data.name);
+
+          // $("#name").text(data.name);
+          // $('#name').attr("src", data.image);
+
+
+
+        });
+    }
+    else
+    {
+      alert("Please fill out all fields before submitting!");
+    }
+
+      return false;
+    });
 
 
 });
