dccd33779561ea4e6e6128194b77749ec94a8bb2
usmanjamil83 <usmanjamil83@hotmail.com>
2017-05-18 19:47:42 -0400

    test

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ io.on('connection', function(socket){
 });
 
 // Syncing our sequelize models and then starting our express app
-db.sequelize.sync({force: true}).then(function() {
-	http.listen(PORT, function() {
+db.sequelize.sync().then(function() {
+	app.listen(PORT, function() {
 		console.log("App listening on PORT " + PORT);
 	});
 });
