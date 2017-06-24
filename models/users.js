de0f1820214566504cd04b5264ed5a6034735f2c
usmanjamil83 <usmanjamil83@hotmail.com>
2017-06-24 13:09:13 -0400

    test

--- models/users.js
+++ models/users.js
@@ -2,92 +2,82 @@ module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     name: {
       type: DataTypes.STRING,
-      validate: {
-       isAlpha: true,
-       notNull: true,
-         //do we need this as well?
-         notEmpty: true,
-         len: [2, 25]
-       },
-       age: {
-         type: DataTypes.INTEGER,
-         validate: {
-           len: [2],
-           min: 18,
-           max: 99,
-           isAlpha: false,
-           notNull: true,
-           notEmpty: true
-         }
-       },
-       gender: {
-         type: DataTypes.STRING,
-         defaultValue: "Male",
-         validate: {
-           isIn: [
-           ["Agender", "Androgynous", "Female", "Male", "Pangender", "Transgender", "Transsexual"]
-           ],
-         }
-       },
-       quote: {
-         type: DataTypes.STRING,
-         defaultValue: "Let's git up!",
-         validate: {
-           notNull: true,
-           isAlphanumeric: true,
-           len: [2, 100],
-         }
-       },
-       image: {
-        //change dt to string to store file path instead?
-        type: DataTypes.STRING,
-        validate: {
-          notNull: true
-        }
-      },
-      answer1: {
-        type: DataTypes.STRING,
-        validate: {
-          notNull: true
-        }
-      },
-      answer2: {
-        type: DataTypes.STRING,
-        validate: {
-          notNull: true
-        }
-      },
-      answer3: {
-        type: DataTypes.STRING,
-        validate: {
-          notNull: true
-        }
-      },
-      answer4: {
-        type: DataTypes.STRING,
-        validate: {
-          notNull: true
-        }
-      },
-      answer5: {
-        type: DataTypes.STRING,
-        validate: {
-          notNull: true
-        }
-      },
-      answer6: {
-        type: DataTypes.STRING,
-        validate: {
-          notNull: true
-        }
-      },
-      matchpoints: {
-        type: DataTypes.INTEGER,
-        validate: {
-          notNull: true
-        }
-      }
-    }
-  });
+     //  validate: {
+     //   notNull: true,
+     //   len: [2, 25]
+     // }
+   },
+   age: {
+     type: DataTypes.INTEGER,
+     // validate: {
+     //   len: [2],
+     //   min: 18,
+     //   max: 99,
+     //   notNull: true
+     // }
+   },
+   gender: {
+     type: DataTypes.STRING,
+     // validate: {
+     //   notNull: true
+     // }
+   },
+   quote: {
+     type: DataTypes.STRING,
+     defaultValue: "Let's git up!",
+     // validate: {
+     //   notNull: true,
+     //   len: [2, 25]
+     // }
+   },
+   image: {
+    type: DataTypes.TEXT,
+    // validate: {
+    //   notNull: true
+    // }
+  },
+  answer1: {
+    type: DataTypes.STRING,
+    // validate: {
+    //   notNull: true
+    // }
+  },
+  answer2: {
+    type: DataTypes.STRING,
+    // validate: {
+    //   notNull: true
+    // }
+  },
+  answer3: {
+    type: DataTypes.STRING,
+    // validate: {
+    //   notNull: true
+    // }
+  },
+  answer4: {
+    type: DataTypes.STRING,
+    // validate: {
+    //   notNull: true
+    // }
+  },
+  answer5: {
+    type: DataTypes.STRING,
+    // validate: {
+    //   notNull: true
+    // }
+  },
+  answer6: {
+    type: DataTypes.STRING,
+    // validate: {
+    //   notNull: true
+    // }
+  },
+  matchpoints: {
+    type: DataTypes.INTEGER,
+    // validate: {
+    //   notNull: true
+    // }
+  }
+});
   return User;
 };
